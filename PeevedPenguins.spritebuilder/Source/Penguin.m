968bf820e4faf3a37997418210d4f0699a01175f
lemonaid119 <adamstone@Adam-Stones-MacBook-Pro.local>
2014-07-16 12:24:45 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Penguin.m
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -9,14 +9,6 @@
 #import "Penguin.h"
 
 @implementation Penguin
-- (id)init {
-    self = [super init];
-    
-    if (self) {
-        CCLOG(@"Penguin created");
-    }
-    
-    return self;
-}
+
 
 @end
