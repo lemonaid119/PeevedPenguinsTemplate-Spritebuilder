1115821779afcbd1ad3627d708fcb94aebd2a5fe
lemonaid119 <adamstone@adam-stones-mbp.wssb.wa.gov>
2014-07-17 07:25:12 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Adam Stone on 7/17/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gameplay : CCSprite
+
+@end
