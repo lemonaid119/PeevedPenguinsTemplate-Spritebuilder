9138581891c0267499da7f2201a77af79e6631a6
lemonaid119 <adamstone@adam-stones-mbp.wssb.wa.gov>
2014-07-19 06:56:52 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -43,12 +43,11 @@
     // ensure followed object is in visible are when starting
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];
+    [_contentNode runAction:follow];
 }
 - (void)retry {
     // reload this level
     [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
-    CCActionFollow *follow = [CCActionFollow actionWithTarget:retry worldBoundary:self.boundingBox];
-    [self runAction:follow];
+
 }
 @end
